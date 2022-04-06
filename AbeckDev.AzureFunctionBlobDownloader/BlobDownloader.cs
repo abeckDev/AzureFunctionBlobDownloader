b679032f4f0511d59473086eb4745c869fc157d3
Alexander Beck <alexander.beck@microsoft.com>
2022-04-06 13:17:28 +0200

    Added Function

---
+++ AbeckDev.AzureFunctionBlobDownloader/BlobDownloader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using AbeckDev.AzureFunctionBlobDownloader.Services;
+using System.Net.Http;
+
+namespace AbeckDev.AzureFunctionBlobDownloader
+{
+    public static class BlobDownloader
+    {
+        [FunctionName("BlobDownloader")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (!req.Query.ContainsKey("blobId"))
+            {
+                return new BadRequestObjectResult("Please provide the blobId query parameter!");
+            }
+
+            string blobId = req.Query["blobId"];
+            var responseStream = await BlobStorageService.GetBlob(blobId);
+
+            return new FileContentResult(responseStream.ToArray(), "application/octet-stream")
+            {
+                FileDownloadName = blobId,
+            }; 
+        }
+    }
+}
+
---
+++ AbeckDev.AzureFunctionBlobDownloader/Services/BlobStorageService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+
+namespace AbeckDev.AzureFunctionBlobDownloader.Services
+{
+	public static class BlobStorageService
+	{
+
+        public static async Task<MemoryStream> GetBlob(string blobName)
+        {
+
+            string ConnectionString = System.Environment.GetEnvironmentVariable("StorageAccountConnectionString");
+            string Container = System.Environment.GetEnvironmentVariable("blobContainer");
+            string debugBlobName = blobName;
+
+            BlobClient blobClient = new BlobClient(ConnectionString,Container, blobName);
+
+            var stream = new MemoryStream();
+            await blobClient.DownloadToAsync(stream);
+            stream.Position = 0;
+
+            return stream;
+            
+        }
+    }
+}
+
